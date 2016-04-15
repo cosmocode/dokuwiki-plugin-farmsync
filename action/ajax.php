d37c07f91f9a6a07352ce09b0dd217486f73022c
Michael Grosse <grosse@cosmocode.de>
2016-04-15 15:30:46 +0200

    Save remote files in farm_util + create correct revisions

--- action/ajax.php
+++ action/ajax.php
@@ -8,6 +8,8 @@
 
 if(!defined('DOKU_INC')) die();
 
+use plugin\farmsync\meta\farm_util;
+
 class action_plugin_farmsync_ajax extends DokuWiki_Action_Plugin {
 
     /**
@@ -18,6 +20,13 @@ class action_plugin_farmsync_ajax extends DokuWiki_Action_Plugin {
      */
     public function register(Doku_Event_Handler $controller) {
         $controller->register_hook('AJAX_CALL_UNKNOWN', 'BEFORE', $this, 'handle_ajax');
+    }
+
+    private $farm_util;
+
+    function __construct()
+    {
+        $this->farm_util = new farm_util();
     }
 
     /**
@@ -32,6 +41,7 @@ class action_plugin_farmsync_ajax extends DokuWiki_Action_Plugin {
         if($event->data != 'plugin_farmsync') return;
         $event->preventDefault();
         $event->stopPropagation();
+
         global $INPUT;
         dbglog('ajax received');
 
@@ -46,25 +56,21 @@ class action_plugin_farmsync_ajax extends DokuWiki_Action_Plugin {
             return;
         }
         $localModTime = filemtime(wikiFN($page));
-        $animalDataDir = DOKU_FARMDIR . $animal . '/data/';
-        $remoteFN = $animalDataDir . 'pages/' . join('/', explode(':', $page)) . '.txt';
-        // fixme: get data dir via farmer helper
+        $animalDataDir = $this->farm_util->getAnimalDataDir($animal);
         if (!$INPUT->has('farmsync-content')) {
-            io_saveFile($remoteFN, io_readFile(wikiFN($page)));
-            touch($remoteFN, $localModTime);
+            $this->farm_util->saveRemotePage($animal, $page, io_readFile(wikiFN($page)), $localModTime);
             header('Content-Type: application/json');
             http_status(200);
             $json = new JSON;
             echo $json->encode("");
+            return;
         }
 
-        $content = $INPUT->str('farmsunc-content');
+        $content = $INPUT->str('farmsync-content');
         $remoteArchiveFileTrunk = $animalDataDir . 'attic/' . join('/', explode(':', $page));
         $remoteArchiveFileName = $remoteArchiveFileTrunk . '.' . $localModTime . '.txt.gz';
-        $remoteChangelog = $animalDataDir . 'meta/' . join('/', explode(':', $page)) . 'change';
         dbglog($remoteArchiveFileName);
         if (file_exists($remoteArchiveFileName)) {
-            $this->freeChangelogRevision($remoteChangelog, $localModTime);
             $filesToIncrement = array($remoteArchiveFileName);
             $i = 1;
             while (file_exists($remoteArchiveFileTrunk . '.' . (intval($localModTime)+$i) . '.txt.gz')){
@@ -77,44 +83,8 @@ class action_plugin_farmsync_ajax extends DokuWiki_Action_Plugin {
                 io_saveFile(join('.',array($fid, intval($frev)+1,$fextension)), io_readFile($file));
             }
         }
-        $this->insertIntoChangelog($remoteChangelog, $localModTime, clientIP(true), DOKU_CHANGE_TYPE_MINOR_EDIT, $page, $INPUT->server->str('REMOTE_USER'), "Revision inserted due to manual merge");
-        io_saveFile($remoteArchiveFileName, io_readFile(wikiFN($page)));
-        io_saveFile($remoteFN, $content); // FIXME: Do we have to trigger the creation of an archive file? Should we trigger page write events? For what user?
-    }
-
-    public function freeChangelogRevision($changelogFile,$rev){
-        $lines = explode("\n",io_readFile($changelogFile));
-        $lineToMakeFree = -1;
-        foreach ($lines as $index => $line) {
-            if (substr($line,0,10) == $rev){
-                $lineToMakeFree = $index;
-                break;
-            }
-        }
-        if ($lineToMakeFree == -1) return;
-
-        do {
-            $parts = explode("\t",$lines[$lineToMakeFree]);
-            array_shift($parts);
-            array_unshift($parts,$rev+1);
-            $lines[$lineToMakeFree] = join("\t",$parts);
-            $lineToMakeFree += 1;
-            $rev += 1;
-        } while (substr($lines[$lineToMakeFree],0,10) == $rev);
-    }
-
-    public function insertIntoChangelog($changelogFile,$rev, $ip, $type, $page, $user, $sum, $extra="") {
-        $lines = explode("\n",io_readFile($changelogFile));
-        foreach ($lines as $index => $line) {
-            if (substr($line,0,10) == $rev) {
-                return false;
-            }
-            if (substr($line,0,10) > $rev) {
-                array_splice($lines, $index, 0, join("\t",array($rev, $ip, $type, $page, $user, $sum, $extra)));
-                return true;
-            }
-        }
-        array_splice($lines, $index, 0, join("\t",array($rev, $ip, $type, $page, $user, $sum, $extra)));
-        return true;
+        $this->farm_util->addRemoteChangelogRevision($animal, $page, $localModTime, clientIP(true), DOKU_CHANGE_TYPE_MINOR_EDIT, $INPUT->server->str('REMOTE_USER'), "Revision inserted due to manual merge");
+        $this->farm_util->replaceRemoteFile($remoteArchiveFileName, io_readFile(wikiFN($page)));
+        $this->farm_util->saveRemotePage($animal, $page, $content);
     }
 }
